b1282191f19e3c16470036444c44b13e83da26d5
EmmanuelRuiz5307 <emmaprofemx@gmail.com>
2022-11-18 22:03:17 -0600

    Creacion de escenario 1,2,4

---
+++ Assets/Scripts/Control_barreras.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_barreras : MonoBehaviour
+{
+    //Atributos
+    [SerializeField]
+    Transform barrera;
+
+    [SerializeField]
+    List<Transform> puntos;
+
+    [SerializeField]
+    int indice_destino;
+
+    [SerializeField]
+    float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barrera.position = Vector3.MoveTowards(barrera.position,
+            puntos[indice_destino].position, velocidad);
+
+        if (Vector3.Distance(barrera.position, 
+            puntos[indice_destino].position) <= 0.1)
+        {
+            indice_destino = ++indice_destino % puntos.Count;
+        }
+    }
+}
